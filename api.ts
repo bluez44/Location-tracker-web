ed6b43a5657d8a2fe4e8f9cb5d18228796ded0fe
Quang Vinh <149250732+bluez44@users.noreply.github.com>
2025-07-06 02:49:30 +0700

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://location-tracker-api-black.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
